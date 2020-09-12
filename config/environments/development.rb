5313ec0edf3dfc4ee5783392f1acee11bc9190e6
Mark Stacy <mbstacy@gmail.com>
2020-09-12 08:27:47 -0700

    add rights statement and date issued and date created

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,7 +55,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_options = { from: ENV['EMAIL_USERNAME'] }
+  config.action_mailer.default_options = { from: ENV['EMAIL_FROM'] }
   config.action_mailer.smtp_settings = {
     address: ENV['EMAIL_HOST'],
     port: ENV['EMAIL_PORT'],
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_options = { from: ENV['EMAIL_FROM'] }
   config.action_mailer.smtp_settings = {
     address: ENV['EMAIL_HOST'],
     port: ENV['EMAIL_PORT'],
