a09b0f8c2424c11ad1822fd7e85184b2130dd565
Mark Stacy <mbstacy@gmail.com>
2020-09-29 13:27:05 -0600

    helper method for link identifier

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+    def link_to_external_url(options={})
+        #logger.debug "Options: #{options[:value]}"
+        link_to("#{options[:value].first()}", "#{options[:value].first()}") 
+    end
 end
